19630692e3c4d19877ef38f19c4726736496da17
Avneet Singh <avneetsingh@Avneets-MacBook-Air.local>
2024-07-07 21:32:02 +0530

    main site redirects to notification

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
-import React from "react";
+"use client";
+import { useRouter } from "next/navigation";
+import React, { useEffect } from "react";
 import "react-toastify/dist/ReactToastify.css";
 
 export default function Home() {
+  const Router = useRouter();
+  useEffect(() => {
+    Router.push("/notification");
+  })
   return (
     <>
     </>
