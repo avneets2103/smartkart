61c8f62f8d1826f9270c8715dcb16dc4374cd6af
Avneet Singh <avneetsingh@Avneets-MacBook-Air.local>
2024-07-07 22:49:57 +0530

    favicon finally

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
           <head>
-            <link rel="icon" href="./favicon.ico" sizes="any" /> 
+            <link rel="icon" href="/icons/favicon.ico" sizes="any" /> 
           </head>
           <body>
           <NextUIProvider>
